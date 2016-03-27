dfd4387a6a59507057808c81d1b74c55895626ee
mishtusorous <mishtusorous@gmail.com>
2016-03-27 21:12:58 +0530

    It is still not working but there are some changes.

--- src/main/java/fun/n/learn/annotation/LogMeCustomAnnotationProcessor.java
+++ src/main/java/fun/n/learn/annotation/LogMeCustomAnnotationProcessor.java
@@ -3,25 +3,23 @@ package fun.n.learn.annotation;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.element.TypeElement;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.tools.Diagnostic;
 
 // List the custom annotations that are supported.
 @SupportedAnnotationTypes({ "fun.n.learn.annotation.LogMeCustomAnnotation" })
 // Extend AbstractProcessor. This will let you process.
 public class LogMeCustomAnnotationProcessor extends AbstractProcessor {
 
-	private final static Logger logger = LoggerFactory
-			.getLogger("fun.n.learn.annotation.LogMeCustomAnnotationProcessor");
-
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations,
 			RoundEnvironment roundEnv) {
-		logger.debug("I was here.");
+
+		Messager messager = processingEnv.getMessager();
+		messager.printMessage(Diagnostic.Kind.NOTE, "I was here.");
 
 		// TODO: Put some meaningful code here. Right now just get it to work.
 
